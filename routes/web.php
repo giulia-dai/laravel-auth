890f3dd4917826a795eacb868d5fbe271b348507
Giulia Carmen Dai <giuliadai1508@gmail.com>
2023-05-22 16:16:25 +0200

    errore admin.dashboard not found

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::middleware(['auth', 'verified'])
     ->name('admin.')
     ->prefix('admin')
     ->group(function () {
-        Route::get('/', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+        Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     });
 
 Route::middleware('auth')->group(function () {
